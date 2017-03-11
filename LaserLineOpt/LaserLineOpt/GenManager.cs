fe742da2da28cf3a3fb695022942407d1df22860
Emmemad <nik1609@yandex.ru>
2017-03-11 10:39:20 +0300

    Started GenManager class

--- LaserLineOpt/LaserLineOpt/GenManager.cs
+++ LaserLineOpt/LaserLineOpt/GenManager.cs
@@ -8,6 +8,17 @@ namespace LaserLineOpt
 {
     class GenManager
     {
+        List<Plate> currentPopulation, newPopulation;
 
+        public GenManager(int sizeOfPopulation)
+        {
+            currentPopulation = new List<Plate>();
+            newPopulation = new List<Plate>();
+        }
+
+        public void MakeNewGenerationCurrent()
+        {
+            List<Plate> tmp;
+        }
     }
 }
